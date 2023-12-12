3f501d774d845455e913242ef877c71cbef8f12f
Sivakornsir <141816219+Sivakornsir@users.noreply.github.com>
2023-12-12 15:37:33 +0700

    Lab8_3

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -11,3 +11,52 @@
 "Brook"
 "Jinbe"
 */
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int age,tall,bounty;
+    string character;
+    cout << "Enter your age: ";
+    cin >> age;
+
+if(age<=25){
+    cout << "Enter your height: ";
+    cin >> tall;
+    if(tall<100){
+        character = "Chopper";
+    }
+    else if (tall<180){
+        character = "Usopp";
+    }
+    else 
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty>1100000000){
+            character = "Zoro";
+        }
+        else {
+            character = "Sanji";
+        }
+    }
+}
+    else if (age<=60)
+    {
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty>500000000){
+            character = "Jinbe";
+        }
+        else {
+            character = "Franky";
+        }
+    }
+else
+{
+    character="Brook";
+}
+cout << "Your character = " << character;
+
+}
